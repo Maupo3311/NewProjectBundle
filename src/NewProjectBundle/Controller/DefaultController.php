21b89ed65fe4e6c37000c0d7d87f5a03b6405a3d
Sergey <Maupo3311@mail.ru>
2019-05-30 13:39:45 +0700

    first commit

--- src/NewProjectBundle/Controller/DefaultController.php
+++ src/NewProjectBundle/Controller/DefaultController.php
@@ -9,13 +9,9 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * Class DefaultController
  * @package NewProjectBundle\Controller
- * @Route("/test")
  */
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/test")
-     */
     public function indexAction()
     {
         return new Response('SUCCESS!');
